e60fffb397f3dd1706869477380b864d8db1de2e
Erfan Rezaei <erfan.rezaei.dev@gmail.com>
2023-09-08 02:51:13 +0330

    General styles updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,28 @@
 import React from "react";
+import Header from "./components/header/Header";
+import About from "./components/about/About";
+import Experience from "./components/experience/Experience";
+import Footer from "./components/footer/Footer";
+import Nav from "./components/nav/Nav";
+import Portfolio from "./components/portfolio/Portfolio";
+import Services from "./components/services/Services";
+import Testimonials from "./components/testimonials/Testimonials";
+import Contact from "./components/contact/Contact";
 
 const App = () => {
-  return <div>Hello!</div>;
+  return (
+    <>
+      <Header />
+      <Nav />
+      <About />
+      <Experience />
+      <Services />
+      <Portfolio />
+      <Testimonials />
+      <Contact />
+      <Footer />
+    </>
+  );
 };
 
 export default App;
